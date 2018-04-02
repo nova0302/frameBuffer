39e605a49ccd5b77581b77537cca377b6fbdc71f
Sanglae Kim <nova0302@hotmail.com>
2018-04-02 13:18:46 +0900

    generate statement added to led_test.v

--- rtl/verilog/led_test.v
+++ rtl/verilog/led_test.v
@@ -5,20 +5,20 @@
 `timescale 1ns/10ps
 module led_test #(parameter NUM_SEG=6)
    (
-    input 		       clk, rst_n, btn,
-    input [3:0] 	       sw,
-    output reg [8*NUM_SEG-1:0] seg
+    input 		   clk, rst_n, btn,
+    input [3:0] 	   sw,
+    output [8*NUM_SEG-1:0] seg
     );
 
 `include "myPkg.v"
-   reg 			       btn_dly;
-   wire 		       catch = ~btn & btn_dly;
+   reg 			   btn_dly;
+   wire 		   catch = ~btn & btn_dly;
    always@(posedge clk)
      btn_dly <= btn;
 
-   reg [3:0] 		       sw_r[0:NUM_SEG-1]; //packed array
+   reg [3:0] 		   sw_r[0:NUM_SEG-1]; //packed array
 
-   integer 		       i;
+   integer 		   i;
    always@(posedge clk)
      if(catch)begin
 	sw_r[0] <= sw;
@@ -26,12 +26,22 @@ module led_test #(parameter NUM_SEG=6)
 	   sw_r[i+1] <= sw_r[i];
 	end
      end
-   
+`ifdef GEN_BLK   
+   genvar k;
+   generate
+      for(k=0; k<NUM_SEG; k=k+1) begin:SEG
+	 assign seg[k*8+:8] = seg_drv(sw_r[k]);
+      end
+   endgenerate
+`else
    integer j;
    always@(*)
      for(j=0; j<NUM_SEG; j=j+1)begin
    	seg[j*8 +: 8] = seg_drv(sw_r[j]);
      end
+   
+`endif
+   
    
 endmodule // led
 
